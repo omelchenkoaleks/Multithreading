bac07e28e8f0acaf8edd86f71c3b061f6d47c83c
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-10-07 16:25:46 +0300

    используется Handler - он обрабатывает полученное сообщение в главном потоке ...

--- app/src/main/java/com/omelchenkoaleks/multithreading/_001_handler/HandlerActivity.java
+++ app/src/main/java/com/omelchenkoaleks/multithreading/_001_handler/HandlerActivity.java
@@ -3,8 +3,11 @@ package com.omelchenkoaleks.multithreading._001_handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.omelchenkoaleks.multithreading.R;
@@ -15,6 +18,8 @@ public class HandlerActivity extends AppCompatActivity {
     private static final String TAG = "HandlerActivity";
 
     private TextView mInfoTextView;
+    private Button mStartButton;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,18 @@ public class HandlerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_001_handler);
 
         mInfoTextView = findViewById(R.id.info_handler_text_view);
+        mStartButton = findViewById(R.id.start_handler_button);
+
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                // обновляем TextView
+                mInfoTextView.setText("number of uploaded files: " + msg.what);
+                if (msg.what == 10) {
+                    mStartButton.setEnabled(true);
+                }
+            }
+        };
     }
 
     public void startOrTestOnClick(View view) {
@@ -47,13 +64,27 @@ public class HandlerActivity extends AppCompatActivity {
                     доступна только из основного потока. Новые потоки не имеют доступа к элементам
                     экрана !!!
                  */
+//                Thread thread = new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        for (int i = 1; i <= 10; i++) {
+//                            downloadFile();
+//                            mInfoTextView.setText("number of uploaded files: " + i);
+//                            Log.d(TAG, "number of uploaded files:" + i);
+//                        }
+//                    }
+//                });
+//                thread.start();
+//                break;
+
+                mStartButton.setEnabled(false); // деактивируем кнопку, чтобы ее нельзя было нажать
                 Thread thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        for (int i = 1; i <= 10; i++) {
+                        for (int i = 1; i <= 10; i ++) {
                             downloadFile();
-                            mInfoTextView.setText("number of uploaded files:" + i);
-                            Log.d(TAG, "number of uploaded files:" + i);
+                            mHandler.sendEmptyMessage(i);
+                            Log.d(TAG, "number of uploaded files: " + i);
                         }
                     }
                 });
